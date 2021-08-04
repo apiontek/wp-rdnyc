a7dbb5b297261fa4959a13149c735406581d596a
Adam Piontek <adam@73k.us>
2021-08-04 11:15:05 -0400

    restore autoloader as constant requirement

--- functions.php
+++ functions.php
@@ -17,12 +17,5 @@ require_once( WP_RDNYC_DIR . '/custom-functions.php' );
 /**
  * Autoloader for browersync
  */
-if ( defined( 'SCRIPT_DEBUG' ) && SCRIPT_DEBUG ) {
-  require_once( WP_RDNYC_DIR . '/vendor/autoload.php' );
-  \A7\autoload( __DIR__ . '/src' );
-} else {
-  require_once( WP_RDNYC_DIR . '/src/classes.php' );
-  require_once( WP_RDNYC_DIR . '/src/enqueue.php' );
-  require_once( WP_RDNYC_DIR . '/src/setup.php' );
-  require_once( WP_RDNYC_DIR . '/src/sidebars.php' );
-}
+require_once( WP_RDNYC_DIR . '/vendor/autoload.php' );
+\A7\autoload( __DIR__ . '/src' );
